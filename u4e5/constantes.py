63690ab867d49ee6fb0ec04fad5e7670025a21c5
cg19 <132721288+cgrp19@users.noreply.github.com>
2023-07-05 03:39:45 -0300

    Add files via upload

---
+++ u4e5/constantes.py
@@ -0,0 +1,7 @@
+API_KEY = "2d704497547a848ed345ee827f51f900"
+URL_G = "https://api.themoviedb.org/3/genre/movie/list?language=en"
+URL_P = "https://api.themoviedb.org/3/discover/movie?api_key="
+HEADERS = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOWY5YzM0Yzc3MGNhZTE2YmI5ZTAzZWE3ZDAwMjE5YyIsInN1YiI6IjY0OTM5OTQ2OWEzNThkMDBhYzRmYmViZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wD4obKs2JQ8Mm0UBmXv0G1DSjzs4QQ9f8PowMaK1AY4"
+}
